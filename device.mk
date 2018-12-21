c150b921819674beaedb886e8882a52502cba595
Lubin Yin <lubiny@codeaurora.org>
2018-12-21 15:21:46 -0800

    violet: Deploy libwfdaac_proprietary for miracast
    Change-Id: I7f00c9f0a0375d7007123614e7605e3915903b90

--- device.mk
+++ device.mk
@@ -457,7 +457,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libqdMetaData \
     libqdMetaData.system \
-    libqdMetaData.vendor
+    libqdMetaData.vendor \
+    libwfdaac_proprietary
 
 PRODUCT_BOOT_JARS += \
     WfdCommon

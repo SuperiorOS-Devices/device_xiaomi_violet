9db8f3550dca1e45ca31014cceac6270042d8171
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2019-06-24 23:23:36 +0530

    violet: Fixup for superior bringup
    - Fixed dependencies.
    - Updated README
    - removed vendorsetup.sh(not needed)
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I14b88d1aae9f9d532090deec5810145d0caf6c80

--- superior_violet.mk
+++ superior_violet.mk
@@ -14,7 +14,6 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Superior stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/superior/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo superior_violet-eng
-add_lunch_combo superior_violet-userdebug

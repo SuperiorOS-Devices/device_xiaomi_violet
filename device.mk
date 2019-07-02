425098c917a49b53199f7b96603bce147b394cb4
Rizky Benggolo <travarilo@gmail.com>
2019-07-02 13:02:32 +0000

    violet: Enable & Fix Smart Charging
    - add device specific init script
    - add sysfs nodes
    - set permissions
    - address denial
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
 
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.target.rc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \

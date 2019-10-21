544ccef6719cc56e1abdadda1c32e8ee95b64467
officialyash007 <officialyash007@gmail.com>
2019-10-21 15:42:51 +0000

    violet: SuperiorOS bringup
    Signed-off-by: officialyash007 <officialyash007@gmail.com>

--- superior_violet.mk
+++ superior_violet.mk
@@ -13,14 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common SuperiorOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_violet
+PRODUCT_NAME := superior_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro

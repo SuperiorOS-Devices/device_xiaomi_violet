e9aa1f723b7509cff072ba67fa39b48f982963de
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2020-01-18 14:19:58 +0200

    violet: Enable TARGET_INCREMENTAL_BLOCK_BASED
    Signed-off-by: officialyash007 <officialyash007@gmail.com>

--- superior_violet.mk
+++ superior_violet.mk
@@ -18,6 +18,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 $(call inherit-product, vendor/superior/config/common.mk)
+TARGET_INCREMENTAL_BLOCK_BASED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := superior_violet

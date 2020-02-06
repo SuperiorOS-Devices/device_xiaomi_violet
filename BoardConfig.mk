55a96436644e75427d21380b711f00b6756a99d3
officialyash007 <officialyash007@gmail.com>
2020-02-06 07:25:42 +0000

    violet: fix a derp *derp while picking 2c1e35cafb9c1536eb9f0e91690ce5fbad0a4321
    Signed-off-by: officialyash007 <officialyash007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,9 +163,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2018-08-05
-
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
 

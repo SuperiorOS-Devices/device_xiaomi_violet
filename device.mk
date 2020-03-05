4a5592f3d4499bdd486eb35ba443d7536863a131
Fenglin Wu <fenglinw@codeaurora.org>
2020-03-05 19:31:14 +0800

    violet: vibrator: Add config to exclude haptics devices from input subsystem
    Haptics devices which registers as input FF devices are not supposed to
    be detected by input subsystem. Add excluded-input-devices.xml file for
    input subsystem to exclude them.
    
    Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692

--- device.mk
+++ device.mk
@@ -409,6 +409,9 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

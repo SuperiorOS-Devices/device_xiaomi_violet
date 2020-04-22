712455058e39c0dad8550018074e08932bbe9837
merothh <merothh@gmail.com>
2020-04-23 02:41:37 +0530

    violet: Decommonize powerhint
    * Remove all ufs related hints as well
    
    Change-Id: I567d8dee140266512434f909d98a90f532288d44

--- device.mk
+++ device.mk
@@ -55,5 +55,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Power
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)

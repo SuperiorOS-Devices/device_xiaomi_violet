903133430fa0de90079bb15ef3e656fdec5b4709
officialyash007 <officialyash007@gmail.com>
2020-06-03 08:42:49 +0000

    violet: update build fingerprint from Coral's June patch
    Signed-off-by: officialyash007 <officialyash007@gmail.com>

--- init/init_violet.cpp
+++ init/init_violet.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
 }

82b1da8fcd15c7978eb5504fa47d70fb349a187b
tesla59 <talktonishantsingh.ns@gmail.com>
2020-06-03 16:10:13 +0530

    violet: drop org.ifaa.android.manager

--- device.mk
+++ device.mk
@@ -236,13 +236,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \

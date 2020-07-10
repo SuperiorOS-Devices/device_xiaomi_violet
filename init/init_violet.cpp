292ec10b1566782607ce62a630a5f96a7700790b
nipin <nipinna0@gmail.com>
2020-07-10 02:46:37 +0000

    violet: update build fingerprint from Coral's July patch

--- init/init_violet.cpp
+++ init/init_violet.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys");
 }

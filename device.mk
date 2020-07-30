393c925e7e6a536978aed8d9695d5a9cde16bcbf
Bharat Pawar <bpawar@codeaurora.org>
2020-07-30 20:18:30 +0530

    violet: vibrator: Enable LED based vibrator
    Enable LED based vibrator for talos, moorea and
    atoll targets.
    
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc

--- device.mk
+++ device.mk
@@ -405,6 +405,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 

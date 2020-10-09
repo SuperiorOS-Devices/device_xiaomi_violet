0d094607da3860019771051dcd957c630d073712
Bruno Martins <bgcngm@gmail.com>
2020-10-09 12:19:37 +0100

    violet: power: Adapt extension to new aidl power HAL
    Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,6 +178,9 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop

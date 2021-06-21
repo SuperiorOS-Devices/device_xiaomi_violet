bb1a552d0dda9a9fd45748e5798079f2fa2f0835
Joker-V2 <nipinna0@gmail.com>
2021-06-21 20:33:17 +0530

    violet: Enable Pixel offline charging animation

--- superior_violet.mk
+++ superior_violet.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/superior/config/common.mk)
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 

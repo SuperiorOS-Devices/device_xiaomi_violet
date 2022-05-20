f2b65741b990d202102f62de8aa4aca8c0ab4300
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:40:27 +0100

    violet: Preopt SystemUI
    Bug: 232860596
    Test: See no dex2oat invocation on the first boot.
    Change-Id: Icc38efb4b702b518caa22a3abfc7c04d6ba1ac2a

--- device.mk
+++ device.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \

c3379f302bf45ded10d62d63b00523ce8f89da44
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    violet: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -186,7 +186,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fingerprint feature
 PRODUCT_COPY_FILES += \

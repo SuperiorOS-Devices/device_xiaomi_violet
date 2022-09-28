f962410715dcf3e479056c2a433c6e8559f30fe3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:38:17 +0200

    violet: Build default health HIDL recovery impl
    Change-Id: I9710760a1b7895ed9a0c3da967c4b30ea655bf94

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL

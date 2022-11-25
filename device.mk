348b42f177f83f03fd5a383749e3376f239f687d
Joker-V2 <nipinna0@gmail.com>
2022-11-25 13:49:33 +0000

    violet: Build android.hardware.bluetooth.audio@2.1-impl again
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \

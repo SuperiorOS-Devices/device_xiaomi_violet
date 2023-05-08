da31b9a37ad25fbdcc523aef2604730f33cd0fe8
adi8900 <adrianszymanski242@gmail.com>
2023-05-08 19:12:53 +0200

    violet: Restore soundtrigger

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.audio@7.1-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     audio.primary.sm6150:32 \
     audio.r_submix.default \
     audio.usb.default \

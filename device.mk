40b6c8c0a9c3087bf1695d7bd5244c608586371e
AmeChanRain <downloadbot007@gmail.com>
2023-10-05 12:26:45 +0000

    violet: Ship with prebuilt libprotobuf-cpp-full
    * libprotobuf-cpp-full-vendorcompat and libprotobuf-cpp-lite-vendorcompat are deprecated
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- device.mk
+++ device.mk
@@ -370,10 +370,12 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
     libprotobuf-cpp-full \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat \
     librmnetctl \
     libxml2
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \

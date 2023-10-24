aca24c0eb76c90c5f88353a51c7bd4fb5fb9d0f8
Chaohui Wang <chaohuiw@google.com>
2023-10-24 23:47:35 +0800

    violet: parts: Migrate to CompoundButton.OnCheckedChangeListener
    Switch and SwitchCompat are both CompoundButton.
    
    Using CompoundButton in Java will helps migration in the future.
    
    Bug: 306658427
    Test: manual - check Settings pages
    Test: m RunSettingsLibRoboTests
    Change-Id: I85a70d4c504d8584030ea4a058f30d74206ab835

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -22,7 +22,8 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import androidx.preference.Preference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -33,10 +34,9 @@ import androidx.preference.SwitchPreference;
 import org.lineageos.settings.R;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 public class DiracSettingsFragment extends PreferenceFragment implements
-        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+        OnPreferenceChangeListener, OnCheckedChangeListener {
 
 
     private static final String PREF_HEADSET = "dirac_headset_pref";
@@ -84,7 +84,7 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         DiracUtils.setEnabled(isChecked);
         mSwitchBar.setChecked(isChecked);
         if (isChecked){

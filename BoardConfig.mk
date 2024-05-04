f25d7f42eb56370e4af1a0387fe946a41b612d72
Joker-V2 <nipinna0@gmail.com>
2024-05-04 05:13:10 +0000

    violet: Remove selinux neverallows flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -215,7 +215,6 @@ include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # WiFi
 BOARD_HOSTAPD_DRIVER := NL80211

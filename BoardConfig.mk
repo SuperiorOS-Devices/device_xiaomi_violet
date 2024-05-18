4d34ca4778077d557612905969d0c5440662c317
Arian <arian.kulmer@web.de>
2024-05-18 15:52:45 +0200

    violet: Provide a static lib for power mode extension
    Change-Id: I3e2cf17bf1e733838e9e18afdf3f888e814593dc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,7 +179,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-xiaomi
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop

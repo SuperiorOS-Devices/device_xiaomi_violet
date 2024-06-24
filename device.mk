b88120b7876bf7006db6a9324b2a247b88ec2aa3
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-24 16:47:29 +0800

    violet: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -428,7 +428,9 @@ AB_OTA_UPDATER := false
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    usb_compositions.conf \
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget@1.2-service-qti
 
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)

6c349517a9a7a80fbb35d6ac58d1d858476e639c
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-04 20:55:55 +0800

    violet: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd

--- device.mk
+++ device.mk
@@ -431,7 +431,7 @@ AB_OTA_UPDATER := false
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)

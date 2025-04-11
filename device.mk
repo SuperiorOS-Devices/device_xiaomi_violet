ad03168473a22711fcb181a865fe4db470973d9a
Joker-V2 <nipinna0@gmail.com>
2025-04-11 07:31:22 +0000

    violet: Don't copy libprotobuf-cpp-full.so

--- device.mk
+++ device.mk
@@ -378,10 +378,6 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor \
     libxml2
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy

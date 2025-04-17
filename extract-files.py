6c9a8502ae6d0bb6af6912e26179a32d2d248267
Joker-V2 <nipinna0@gmail.com>
2025-04-17 11:10:50 +0000

    violet: extract: Use patchelf v0_9 by default

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,

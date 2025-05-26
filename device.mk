1937935ce2c0e5ea63492b45bbbdaa575133d10b
Michael Bestas <mkbestas@lineageos.org>
2025-05-26 16:37:17 +0300

    violet: Enable charging control bypass mode
    Change-Id: Icd4955df45007b693cf8f01c11cc00edec3888d1

--- device.mk
+++ device.mk
@@ -313,12 +313,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
-
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor

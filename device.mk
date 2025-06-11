62f627064f83858eba3a18b1e681a3a75270cff5
Joker-V2 <nipinna0@gmail.com>
2025-06-11 15:27:34 +0000

    violet: Move away from livedisplay

--- device.mk
+++ device.mk
@@ -294,11 +294,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default

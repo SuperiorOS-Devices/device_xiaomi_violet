40e176be153771a1093620e951816cbaa977a926
Joker-V2 <nipinna0@gmail.com>
2025-06-15 01:02:47 +0000

    violet: Remove deprecated ANT+ targets
    - No longer needed (https://review.lineageos.org/c/LineageOS/android/+/434679)
    - Drop prebuilt com.dsi.ant@1.0-impl.so as well
    
    Change-Id: I5290e3f6f3669bd1d5852202835778cd6bf74318

--- device.mk
+++ device.mk
@@ -129,11 +129,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \

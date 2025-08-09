6b90c735be5d976c7df907cd3270a622d3d633cb
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    violet: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,9 +102,8 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 USE_OPENGL_RENDERER := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml

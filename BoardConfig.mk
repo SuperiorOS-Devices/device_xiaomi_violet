1879fae865c423fa6d7569deb052fdf167a61996
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2025-09-08 12:05:59 +0200

    violet: Include Dolby Atmos
    * Using dolby from: https://github.com/Joker-V2/android_hardware_dolby
    
    [1]: violet: Add dolby volume listener effect
    [2]: violet: audio: Bring all stream effects
    [3]: violet: audio: Sync with Spatial Audio
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,9 +104,9 @@ USE_OPENGL_RENDERER := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
 # Input
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 96

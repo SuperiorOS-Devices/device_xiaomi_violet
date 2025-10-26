124991216f8c41bfc28af0a78735f991bad7d5be
Joker-V2 <nipinna0@gmail.com>
2025-10-26 09:42:13 +0100

    violet: parts: Strip-out always on display

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -34,6 +33,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
 	DiracUtils.initialize(context);
-        DozeUtils.checkDozeService(context);
     }
 }

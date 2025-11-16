67b460ead93ba8f9af7aad0793d48c063fd93ab7
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    violet: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    
    Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0

--- device.mk
+++ device.mk
@@ -130,6 +130,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
